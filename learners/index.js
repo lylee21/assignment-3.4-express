7896b9fa7f5f7c27ef359430137c1057f5d86382
QuirkiDigital <46080052+QuirkiDigital@users.noreply.github.com>
2021-10-31 19:10:45 +0800

    Assignment 3.4 (semi-complete)

---
+++ learners/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+//I'm not sure if I should be putting the values here
+const vehicles = [
+    { "type":"Taxi", "carPlateNo":"SHA1111Z" }
+];
+
+//I'm not sure if I should be putting the values here
+const garages = [
+    { "garageNo":1, "address":"Address 1" }
+];
+
+app.post("/vehicle", function (req, res) {
+    if (vehicles.length > 0 && vehicles.some((v) => v.carPlateNo === req.body.carPlateNo)) {
+      res.status(409);
+      return res.send("Vehicle already exists");
+    }
+    vehicles.push(req.body);
+    res.json(req.body);
+});
+
+app.get("/vehicle", function (req, res) {
+    res.json(vehicles);
+});
+
+app.put("/vehicle/:carPlateNo", function (req, res) {
+    for (v of vehicles) {
+      if (v.carPlateNo === req.body.carPlateNo) {
+        v.type = req.body.type
+        res.status(200);
+        return res.send("Car plate number updated successfully");
+      }
+    }
+    res.status(404);
+    res.send("Car plate number not found");
+});
+
+app.post("/garage", function (req, res) {
+    if (garages.length > 0 && garages.some((g) => g.garageNo === req.body.garageNo)) {
+        res.status(200);
+        return res.send("Success");
+      }
+    garages.push(req.body);
+    res.json(req.body);
+});
+
+app.get("/garage/:garageNo", function (req, res) {
+    res.json(garages);
+});
+
+app.listen(3000);
